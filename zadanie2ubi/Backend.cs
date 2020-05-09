3cac824092505deb1f0c3e4ccc7f000a370a6dad
admin <admin@MacBook-Pro-admin.local>
2020-05-09 19:39:50 +0200

    Initial commit

---
+++ zadanie2ubi/Backend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace zadanie2ubi
+{
+    public class Backend
+    {
+        private static readonly Lazy<Backend>
+        lazy =
+        new Lazy<Backend>
+            (() => new Backend());
+
+        public static Backend Instance { get { return lazy.Value; } }
+
+        private Backend()
+        {
+        }
+    }
+}
